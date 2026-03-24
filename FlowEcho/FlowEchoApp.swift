171ebaeead3e5174b67c3bc76a0bc74f1337c035
luke <mhliluke@gmail.com>
2026-03-24 10:47:33 +0800

    Initial Commit

---
+++ FlowEcho/FlowEchoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowEchoApp.swift
+//  FlowEcho
+//
+//  Created by Luke on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct FlowEchoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
